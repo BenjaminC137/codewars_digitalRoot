9cd957ffad559ecc9978d55f6fb4e290ea2f8629
benjaminc137 <benjamin.salasek@icloud.com>
2018-07-06 10:58:47 -0500

    initial commit

---
+++ codewars_digitalRoot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace codewars_digitalRoot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
